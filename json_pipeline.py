f36797a153cdfa037b230c159171da3b99e2b06e
Mohammadreza Pakzadian <50163984+mrp-78@users.noreply.github.com>
2021-02-28 16:51:02 +0330

    Update json_pipeline.py

--- json_pipeline.py
+++ json_pipeline.py
@@ -1,10 +1,9 @@
 from datetime import datetime
 from itemadapter import ItemAdapter
-from datetime import datetime.now
-
+from datetime import datetime as dt
 class JsonPipeline(object):
     def __init__(self):
-        dt_string = now.strftime("%Y/%m/%d-%H:%M:%S")
+        dt_string = dt.now().strftime("%Y/%m/%d-%H:%M:%S")
         self.file = open("games" + dt_string + ".json", 'wb')
         self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
         self.exporter.start_exporting()
